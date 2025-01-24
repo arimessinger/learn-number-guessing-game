11a35555a8dda572e6bfd3e0132adac2bf5bd8f1
Ari Messinger <amessinger@student.leffellschool.org>
2025-01-24 08:04:48 +0000

    fix: add database connection and username support

--- number_guess.sh
+++ number_guess.sh
@@ -3,5 +3,8 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$((1 + $RANDOM % 1000))
+
+echo -e "Please enter your username\n"
 read USERNAME
-SET_USERNAME=$($PSQL )
+
+SET_USERNAME=$($PSQL "INSERT INTO users(username) VALUES($USERNAME)")
